15faed08a3670825d27434333cea142a3c9bf90c
safia bano <safiabano736@gmail.com>
2024-08-21 12:19:49 +0500

    Assignment9

---
+++ app.js
@@ -0,0 +1,155 @@
+// CH 9
+
+// Q1
+
+// let userCity = prompt("Enter your city name","Karachi")
+// if(userCity === "Karachi"){
+//     alert("Welcome to City of Light")
+// }
+
+// Q2
+
+
+// let gender = prompt("Enter your gender")
+// if(gender === "female"){
+//     alert("Good morning Ma'am")
+// }else if(gender=="male"){
+//     alert("Good morning Sir")
+// }else{
+//     alert("Good morning "+ gender)
+// }
+
+// Q3
+
+// let signal = prompt("Enter your traffic color")
+// if(signal === "Red"){
+//     alert("Must Stop")
+// }else if (signal === "Yellow") {
+//     alert("Ready to move")
+    
+// } else if(signal === "Green"){
+//     alert("Move now")
+    
+// }
+
+// Q4
+
+
+// let fuel = prompt("Enter your fuel liter" ,"0.25")
+// if(fuel === "0.25"){
+//     alert("Please refill the fuel in your car")
+// }
+
+
+// Q5
+
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+//     if("car" < "cat"){
+//         alert("car is smaller than cat");
+//         }    
+    
+
+// Q4
+
+// let marks = prompt("Enter your marks")
+// if (marks >=80 ){
+// alert("A-one\nExcellent")
+// }else if(marks >=70){
+// alert("A\nGood")
+// }else if(marks >=60){
+//     alert("B\nYou need to improve")
+//     }else if(marks <60){
+//         alert("Fail\nSorry")
+//         }
+
+// Q5
+
+// let secNumber = +prompt("Enter a number")
+// if (secNumber === 3){
+//     alert("Bingo! Correct answer ")
+// }else if(secNumber === 3+1){
+//     alert("Close enough to the correct answer")
+// }
+
+// Q6
+
+// let num = prompt("enter a numberf divible by 3")
+// if(num % 3 ===  0){
+//     alert("Yes it's divisisble by 3")
+// }
+// Q7
+
+// let num = prompt("Enter a number")
+// if (num % 2 === 0|| num % 4 === 0){
+//     alert("This is Even Number")
+// }else if(num % 3 === 0 || num % 5 ===0){
+//     alert("This is Odd Number")
+
+// }
+// Q8
+
+
+// let tempr = +prompt("Enter your number","41")
+// if(tempr >= 40){
+//     alert("It is too hot outside.")
+// }else if(tempr >= 30 && tempr < 40){
+//     alert("The Weather today is Normal.")
+// }else if(tempr >= 20  && tempr < 30){
+//     alert("Today’s Weather is cool.")
+// }else if(tempr >= 10  && tempr < 20){
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+
+
+// Q9
+
+// let num1 =+prompt("Enter a number1 value")
+// let operInput = prompt ("Any one select +, -, *, /, %") 
+// let num2 =+prompt("Enter a number2 value")
+// if (operInput == "+"){
+//     alert(num1+num2)
+// }else if (operInput == "-"){
+//     alert(num1-num2)
+// }else if (operInput == "/"){
+//     alert(num1/num2)
+// }else if (operInput == "*"){
+//     alert(num1*num2)
+// }else if (operInput == "%"){
+//     alert(num1%num2)
+// }
+
+
+// Assignment # 9-10 completed
